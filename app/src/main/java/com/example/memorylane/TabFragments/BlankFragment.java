e3ff905a9d7079a887c4b15ca63a5aa99889c96f
Nelu99 <nelu.maciuca99@e-uvt.ro>
2020-04-28 22:10:00 +0300

    improved interface + working light mode and settings fragment / class with local storage

--- app/src/main/java/com/example/memorylane/TabFragments/BlankFragment.java
+++ app/src/main/java/com/example/memorylane/TabFragments/BlankFragment.java
@@ -2,22 +2,13 @@ package com.example.memorylane.TabFragments;
 
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import androidx.preference.PreferenceFragmentCompat;
 
 import com.example.memorylane.R;
 
-public class BlankFragment extends Fragment {
-
-    public BlankFragment() {
-    }
-
+public class BlankFragment extends PreferenceFragmentCompat {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.fragment_blank, rootKey);
     }
 }
